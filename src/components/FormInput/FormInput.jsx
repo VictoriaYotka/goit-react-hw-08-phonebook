b08e711c20013955971f371a017373a6434952f5
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-07-03 12:20:42 +0300

    debug form styles

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,13 +1,13 @@
 import { FormLabel, Input } from '@chakra-ui/react'
 import React from 'react'
-import { input } from './FormInputStyles'
 
 const FormInput = ({type, name, label, pattern, title, styles}) => {
   return (
     <>
     <FormLabel htmlFor={name}  sx={styles}>{label}</FormLabel>
         <Input
-          sx={{styles}, input}
+          sx={styles}
+          mb='16px'
           type={type}
           name={name}
           id={name}
--- src/components/FormInput/FormInputStyles.js
+++
@@ -1,3 +0,0 @@
-export const input = {
-    marginBottom: '16px'
-}
